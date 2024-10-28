5d983d79131bf120e30eee00600917d1cb2c18fd
aokisinn <a.sinn1192@gmail.com>
2024-10-28 12:16:28 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from "next/image";
+'use client'
+
+import { useRef, useState } from "react";
 import styles from "./page.module.css";
 
+type Theme = 'light' | 'dark'
+
+const iframeUrl = 'https://iframe-sample-send-fhdkoj22j-aokisinns-projects.vercel.app/'
+
 export default function Home() {
+  const [theme, setTheme] = useState<Theme | null>(null)
+  const iframeRef = useRef<HTMLIFrameElement>(null)
+
+  const getTheme = () => {
+    console.log('getTheme')
+    console.log(iframeRef.current?.contentWindow?.postMessage({ type: 'getTheme' }, '*'))
+    const theme = iframeRef.current?.contentWindow?.postMessage({ type: 'getTheme' }, '*')
+
+    console.log('theme')
+    console.log(theme)
+    setTheme(theme || "light")
+  }
+
+  const updateTheme = (newTheme: 'light' | 'dark') => {
+    iframeRef.current?.contentWindow?.postMessage({ type: 'updateTheme', data: newTheme }, '*')
+    setTheme(newTheme)
+  }
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className={styles.title}>
+          Theme Receiver Page {theme}
+        </h1>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div>
+          <button onClick={() => getTheme()}>Get Theme</button>
+          <button onClick={() => updateTheme('light')}>Set Light</button>
+          <button onClick={() => updateTheme('dark')}>Set Dark</button>
         </div>
+
+        <iframe
+          ref={iframeRef}
+          src={iframeUrl}
+
+        ></iframe>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
